33a22c7d3a3e388e86b7926fb3d355fd57a6ba0a
Алигатор Крокодилов <studentmtuci1bik25171@gmail.com>
2026-04-10 20:51:37 +0300

    add test3

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World! test1 test 2</h1>')
+  res.send('<h1>Hello World! test1 test 2 test3</h1>')
 })
 
 const PORT = 8080
